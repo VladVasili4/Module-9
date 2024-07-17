4172fa24b6f8daa330685f6171146d9a56d50288
VladVasili4 <dipol777@ya.ru>
2024-07-17 07:54:11 +0300

    Add files via upload

--- homework_9-3.py
+++ homework_9-3.py
@@ -3,10 +3,7 @@ second = ['Строка', 'Урбан', 'Компьютер']
 
 first_result = (len(i[0]) - len(i[1]) for i in zip(first, second) if len(i[0]) != len(i[1]))
 print(list(first_result))
-second_result = (len(x) == len(y) for x in first for y in second)
-print(list(second_result))
 
-print(range(len(first)))
-x = list(range(len(first)))
-print(x)
+second_result = (len(x) == len(y) for x in first for y in second if first.index(x) == second.index(y))
+print(list(second_result))
 

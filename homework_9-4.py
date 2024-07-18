05298df58a5b08390c659169f2b0d56ddb48e0ac
VladVasili4 <dipol777@ya.ru>
2024-07-18 07:17:23 +0300

    Add files via upload

--- homework_9-4.py
+++ homework_9-4.py
@@ -1,4 +1,5 @@
 import io
+from random import choice
 
 first = 'Мама мыла раму'
 second = 'Рамена мало было'
@@ -13,10 +14,22 @@ def get_advanced_writer(file_name):
     return write_everything
 
 
-
 write = get_advanced_writer('example.txt')
-write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
+write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'], 'Это конец')
 
+class  MysticBall:
+    def __init__(self, words):
+        self.words = words
+
+    def __call__(self, *args):
+        self.choice = choice(self.words)
+        return self.choice
+
+
+first_ball = MysticBall(['Это строчка', 'А', 'это', 'уже', 'число', '5', 'в', 'списке', 'Это конец'])
+print(first_ball())
+print(first_ball())
+print(first_ball())
 
 
 

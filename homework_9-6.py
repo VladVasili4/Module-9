684cb862b96a83c9b5fd750c69394838745b4fd8
VladVasili4 <dipol777@ya.ru>
2024-07-25 18:55:21 +0300

    Add files via upload

--- homework_9-6.py
+++ homework_9-6.py
@@ -1,13 +1,23 @@
 def all_variants(text):
-    i = '0'
-    while i in text:
-        yield i
-        i += 1
-
+    n = len(text)
+    y = 1
+    for x in range(n):
+        yield text[x]
+        x += 1
+    y += 1
+    for x in range(n):
+        if y < n + 1:
+            yield text[x:y]
+            x += 1
+            y += 1
+    y = 3
+    for x in range(n):
+        if y < n + 1:
+            yield text[x:y]
+            x += 1
+            y += 1
 
 
 a = all_variants("abc")
 for i in a:
     print(i)
-
-print(a)
